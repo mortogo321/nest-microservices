325370bb8e134e89196898e8c1fe041183e1cd9e
Mor <mortogo321@gmail.com>
2023-04-04 02:02:37 +0700

    fix joi module with es6 issue

--- apps/orders/src/orders.module.ts
+++ apps/orders/src/orders.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { OrdersController } from './orders.controller';
 import { OrdersService } from './orders.service';
 import { ConfigModule } from '@nestjs/config';
-import * as Joi from 'joi';
+import Joi from 'joi';
 import { DatabaseModule } from '@app/common';
 import { OrderRepository } from './orders.repository';
 import { MongooseModule } from '@nestjs/mongoose';
